5ed4a6413ad664b9268bb78d7f4bd5142f3d4f56
barretodanny <dannybarreto22@gmail.com>
2024-02-27 10:17:52 -0500

    Lobby interface, Pages and MessageType enums

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -4,6 +4,28 @@ export interface User {
   lobby: string;
 }
 
+export interface Lobby {
+  lobbyId: string;
+  creatorId: string;
+  lobbyName: string;
+  connectedUsers: string[];
+  lobbyState: string;
+  user1Score: string;
+  user2Score: string;
+  lightState: string;
+  gameTimer: string;
+}
+
+export enum Pages {
+  LANDING_PAGE = "LANDING_PAGE",
+  MAIN_LOBBY = "MAIN_LOBBY",
+  LOBBY = "LOBBY",
+}
+
 export enum MessageType {
+  GET_SELF = "GET_SELF",
+  GET_CONNECTED_USERS = "GET_CONNECTED_USERS",
   UPDATE_USERNAME = "UPDATE_USERNAME",
+  GET_LOBBIES = "GET_LOBBIES",
+  CREATE_LOBBY = "CREATE_LOBBY",
 }

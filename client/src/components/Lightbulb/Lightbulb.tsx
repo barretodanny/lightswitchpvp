0d353d5ad9c01bc2f21779c0fad06b2b06c4061d
barretodanny <dannybarreto22@gmail.com>
2024-03-25 16:52:29 -0400

    LightBulb component and styling

--- client/src/components/Lightbulb/Lightbulb.tsx
+++ client/src/components/Lightbulb/Lightbulb.tsx
@@ -1,18 +1,45 @@
 import styles from "./Lightbulb.module.css";
 
 interface LightbulbProps {
-  on: boolean;
+  color: string;
 }
 
-function Lightbulb({ on }: LightbulbProps) {
+function getColorStyleClass(color: string) {
+  switch (color) {
+    case "red":
+      return { color: "red", lightEffect: "redLightEffect" };
+    case "yellow":
+      return { color: "yellow", lightEffect: "yellowLightEffect" };
+    case "blue":
+      return { color: "blue", lightEffect: "blueLightEffect" };
+    case "green":
+      return { color: "green", lightEffect: "greenLightEffect" };
+    case "orange":
+      return { color: "orange", lightEffect: "orangeLightEffect" };
+    case "purple":
+      return { color: "purple", lightEffect: "purpleLightEffect" };
+    case "white":
+      return { color: "white", lightEffect: "whiteLightEffect" };
+    case "pink":
+      return { color: "pink", lightEffect: "pinkLightEffect" };
+    default:
+      return { color: "yellow", lightEffect: "yellowLightEffect" };
+  }
+}
+
+function Lightbulb({ color }: LightbulbProps) {
+  const colorStyle = getColorStyleClass(color);
+
   return (
     <div className={styles.lightbulb}>
       <div
-        className={`${styles.bulb} ${on && `${styles.on}`} ${
-          on && `${styles.lightEffect}`
+        className={`${styles.bulb} ${styles[colorStyle.color]} ${
+          styles[colorStyle.lightEffect]
         }`}
       >
-        <div className={`${styles.bulbBottom} ${on && `${styles.on}`}`}></div>
+        <div
+          className={`${styles.bulbBottom} ${styles[colorStyle.color]}`}
+        ></div>
       </div>
       <div className={styles.base}></div>
     </div>
